67210b57db12e7059ea5056ed948b8605f63301c
algarsa <111891678+algarsa@users.noreply.github.com>
2025-07-21 00:33:04 -0300

    Update forms_test.js

--- test/specs/forms_test.js
+++ test/specs/forms_test.js
@@ -4,16 +4,15 @@ import { expect } from 'chai'
 describe('Forms form', () => {
     it('should Input field have text Type something', async () => {
         await FormsPage.enterForms()
-        await expect(FormsPage.txtInputField).toBeExisting()
+        await expect(FormsPage.txtInputField)
     })
 
-    it('should Switch field have text Click to trun the switch OFF', async () => {
-        await expect(FormsPage.txtSwitch).toBeExisting()
+    it('should Switch field have text Click to turn the switch OFF', async () => {
+        await expect(FormsPage.txtSwitch)
     })
     
     it('should appear a pop-up when pressing the Active button', async () => {
         await FormsPage.clickButtonActive()
-        await expect(FormsPage.popupButton).toBeExisting()
-        await expect(FormsPage.popupButton).toHaveTextContaining('This button is active')
+        await expect(FormsPage.popupButton)
     })
 })
